7ff2b62228ba0b51c4bfcb825ef4df96924f3faa
sosotheso <64717767+sosotheso@users.noreply.github.com>
2022-08-01 00:43:48 +0200

    erros and exception solutions

---
+++ errors and exceptions/exception.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+t = int(input())
+for j in range(t):
+    try:
+        a, b = input().split()
+        print(int(a) // int(b))
+    except ZeroDivisionError as e:
+        print("Error Code:", e)
+    except ValueError as v:
+        print("Error Code:", v)
